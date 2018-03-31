46eb65621b1fde479e0a7afcfff6980f3a39fec6
tanviriqbal <tanviriqbal4u@live.com>
2018-03-31 07:38:49 +0600

    Variable Mutation

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
-// Type Coercion
+// Variable Mutation
 
 var name = 'Tanvir Iqbal';
 var age = 29;
 
-console.log(name + ' is ' + age + ' years old.'); // JS automatically converts age variable to string.
+console.log(name + ' is ' + age + ' years old.');
+
+age = 'Twenty Nine';
+
+console.log(name + ' is ' + age + ' years old.'); // You can assign one type of value to another type of variable.
 
