bccd05d14001d2c36ed7b259c2fc715a074edf17
tanviriqbal <tanviriqbal4u@live.com>
2018-03-31 09:27:41 +0600

    Coding Challange 1

--- scripts.js
+++ scripts.js
@@ -1,17 +1,27 @@
-// switch
+// coding challange
 
-var job = prompt('What does Tanvir do?');
+var playerOneHeight = 500;
+var playerOneAge = 20;
 
-switch(job.toLowerCase()) {
-    case 'programmer':
-        alert('Tanvir is a ' +job);
-        break;
-    case 'driver':
-        alert('Tanvir is a ' + job);
-        break;
-    default:
-        alert('Tanvir does something else. (' + job + ')');
+var playerTwoHeight = 500;
+var playerTwoAge = 20;
+
+var playerThreeHeight = 500;
+var playerThreeAge = 20;
+
+var playerOneResult = playerOneHeight + playerOneAge * 5;
+var playerTwoResult = playerTwoHeight + playerTwoAge * 5;
+var playerThreeResult = playerThreeHeight + playerThreeAge * 5;
+
+if(playerOneResult > playerTwoResult && playerOneResult > playerThreeResult) {
+    console.log('Player One Wins with ' + playerOneResult + ' points!');
 }
-
-
-
+else if(playerTwoResult > playerOneResult && playerTwoResult > playerThreeResult) {
+    console.log('Player Two Wins with ' + playerTwoResult + ' points!');
+}
+else if(playerThreeResult > playerOneResult && playerThreeResult > playerTwoResult) {
+    console.log('Player Three Wins with ' + playerThreeResult + ' points!');
+}
+else {
+    console.log('Draw');
+}
