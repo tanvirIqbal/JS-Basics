6b9d77849695a427f04093e63250ccfc92e5a28a
tanviriqbal <tanviriqbal4u@live.com>
2018-03-31 06:55:08 +0600

    Data Types and Variables

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
-console.log('Hello World!');
+// Data Types and Variables
+
+// String: Sequence of character, used for text
+
+var name = 'Tanvir Iqbal';
+console.log(name);
+
+// Number: Floating points numbers, for decimal and integers.
+
+var age = 29;
+console.log(age);
+
+// Boolean: Logical data types that can only be true or false
+
+var isMarried = false;
+console.log(isMarried);
+
+// Undefined: variable is declared but not initialized
+
+var isInLove;
+console.log(isInLove);
+
+// Null: Null means an empty or non-existent value. Null is assigned, and explicitly means nothing.
+
+var eyeColor = null;
+console.log(eyeColor);
