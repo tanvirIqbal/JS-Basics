d9cae211afa1c661141a93318270000c74d576f5
tanviriqbal <tanviriqbal4u@live.com>
2018-03-31 07:35:24 +0600

    Type Coercion

--- scripts.js
+++ scripts.js
@@ -1,30 +1,7 @@
-// Some string operation
-
-// String: Sequence of character, used for text
+// Type Coercion
 
 var name = 'Tanvir Iqbal';
-console.log(name);
+var age = 29;
 
-// String Interpolation
-var nickName = 'the bookworm traveller';
-console.log(`Nickname: ${nickName}`);
-
-// Template Literals
-var multiLines = `Look how
-    cool this string is.
-    It is on multiple lines!`;
-console.log(multiLines);
-
-// Lower Case
-console.log(name.toLowerCase());
-
-// Upper Case
-console.log(name.toUpperCase());
-
-// Replace
-console.log(name.replace('Tanvir','Tafhim')); // It returns a new sentence, not works on name variable.
-
-// Includes
-console.log(name.includes('Tanvir')); // True
-console.log(name.includes('Tafhim')); // False
+console.log(name + ' is ' + age + ' years old.'); // JS automatically converts age variable to string.
 
