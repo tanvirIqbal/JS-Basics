5841c16d3d3edf518d58a10bf5a87c3eb47b9156
tanviriqbal <tanviriqbal4u@live.com>
2018-04-04 06:42:05 +0600

    Function Declaration/Statement vs. Function Expression

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// Functions
+// Functions (DRY - Dont Repeat Yourself)
 
 function calculateAge(yearOfBirth) {
     return 2018 - yearOfBirth;
@@ -25,3 +25,34 @@ function yearsUntilRetirement(name, yearOfBirth) {
 yearsUntilRetirement('Tanvir', 1989);
 yearsUntilRetirement('Tafhim', 1992);
 yearsUntilRetirement('Amjad', 1952);
+
+// Function Declaration/Statement - performs actions
+function functionName(parameter) {
+    // Code
+}
+
+
+// Function expression - produce value
+var functionName = function(parameter) {
+    // Code
+}
+
+/*
+DIFFERENCE----
+They're actually really similar. How you call them is exactly the same.The difference lies in how the browser loads them into the execution context.
+
+Function declarations load before any code is executed.
+
+Function expressions load only when the interpreter reaches that line of code.
+
+So if you try to call a function expression before it's loaded, you'll get an error! If you call a function declaration instead, it'll always work, because no code can be called until all declarations are loaded.
+
+Example: Function Expression
+
+alert(foo()); // ERROR! foo wasn't loaded yet
+var foo = function() { return 5; } 
+Example: Function Declaration/Statement
+
+alert(foo()); // Alerts 5. Declarations are loaded before any code can run.
+function foo() { return 5; } 
+*/
