4e3b311d22634f9d33f83dee2adb194b60496f28
tanviriqbal <tanviriqbal4u@live.com>
2018-04-10 06:13:40 +0600

    Objects and Properties

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
-// Arrays
+// Objects and Properties
 
-// Declaration
-var names = ['Tanvir', 'Tafhim', 'Jamil', 'Fahim'];
-var dobs = new Array(1989, 1992, 1991, 1990);
-
-// Getting single value
-console.log(dobs[1]);
-
-var tanvir = ['Tanvir', 'Iqbal', 1989, 'Single'];
-tanvir.push('Green'); // Add an element at the end.
-tanvir.unshift('Mr.'); // Add an element at the start.
-tanvir.pop(); // Remove an element from the end.
-tanvir.shift(); // Remove an element from the start.
-console.log(tanvir);
-
-/*
-indexOf(element) returns index number of an element.
-If the element not found in the array than returns -1
-*/
-if(tanvir.indexOf('Merried') === -1) {
-    console.log('Tanvir is single');
+var tanvir = {
+    firstName: 'Tanvir',
+    lastName: 'Iqbal',
+    yeaarOfBirth: 1989,
+    job: 'Developer',
+    isMarried: false
 }
+
+console.log(tanvir);
+console.log(tanvir.lastName); // 1st way to access
+console.log(tanvir['job']); // 2nd way to access
+
+var tafhim = new Object();
+tafhim.firstName = 'Tafhim';
+tafhim.lastName = 'Iqbal';
+tafhim['job'] = 'Auditor';
+tafhim['yearOfBirth'] = 1992;
+
+console.log(tafhim);
+
+
